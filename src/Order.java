7b996c32b9e8a3dc00aa4a2ceae1231048fa82c7
Moukaskon <moukaskon04@gmail.com>
2025-03-31 11:45:47 +0300

    Extract again

--- src/Order.java
+++ src/Order.java
@@ -6,18 +6,15 @@ public class Order {
     private double totalPrice;
 
     private CustomerInfo customerInfo;
-
-    private String paymentMethod;
-    private boolean paymentStatus;
+    private PaymentProcessor paymentProcessor;
 
     public Order(String orderId, List<String> items, double totalPrice,
-                CustomerInfo customerInfo, String paymentMethod, boolean paymentStatus) {
+                 CustomerInfo customerInfo, PaymentProcessor paymentProcessor) {
         this.orderId = orderId;
         this.items = items;
         this.totalPrice = totalPrice;
         this.customerInfo = customerInfo;
-        this.paymentMethod = paymentMethod;
-        this.paymentStatus = paymentStatus;
+        this.paymentProcessor = paymentProcessor;
     }
 
     public void printOrderDetails() {
@@ -25,16 +22,6 @@ public class Order {
         System.out.println("Items: " + items);
         System.out.println("Total Price: $" + totalPrice);
         customerInfo.printCustomerInfo();
-        processPayment();
-    }
-
-    public void processPayment() {
-        if (!paymentStatus) {
-            System.out.println("Processing payment via " + paymentMethod);
-            paymentStatus = true;
-            System.out.println("Payment successful!");
-        } else {
-            System.out.println("Payment already completed.");
-        }
+        paymentProcessor.processPayment();
     }
 }
